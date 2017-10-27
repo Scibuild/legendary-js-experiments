cf0fcaa433ac4d9d13e65eb5ee3bd52a1ec988aa
Scibuild <zmbx100@gmail.com>
2017-10-27 13:10:59 +1100

    comments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 function setup() {
+  //create canvas the same size as the window
   createCanvas(window.innerWidth, window.innerHeight);
   noStroke();
   background(0);
 }
 function draw() {
-  //background(0);
-
+  //set fill colour based on position
   fill(map(mouseX,0,width, 0, 255, true),
           map(mouseY,0,height, 0, 255, true), 128);
+  //draw ellipse at mouse position
   ellipse(mouseX,mouseY,80,80);
 }
