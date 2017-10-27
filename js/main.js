c10c381928934d713c304b753ee5127f6d68a88b
Scibuild <zmbx100@gmail.com>
2017-10-27 13:05:05 +1100

    Colourful circles

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  noStroke();
+  background(0);
+}
+function draw() {
+  //background(0);
+
+  fill(map(mouseX,0,width, 0, 255, true),
+          map(mouseY,0,height, 0, 255, true), 128);
+  ellipse(mouseX,mouseY,80,80);
+}
