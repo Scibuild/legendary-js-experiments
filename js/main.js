b2a5174bd402187557ebf5029e94eb0152fe10eb
sox2020 <32762690+sox2020@users.noreply.github.com>
2017-11-02 10:39:32 +1100

    Added Button

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var spinner;
 var speed = 10;
 var rotation = 0;
 var drag = 0.995;
+var button;
 
 function setup() {
   //create canvas the same size as the window
@@ -9,19 +10,22 @@ function setup() {
   spinner = loadImage("/assets/spinner.png");
   noStroke();
   angleMode(DEGREES);
-  //background(0);
 
+  button = createButton('click me');
+  button.position(19, 19);
+  //background(0);
+  button.mousePressed(function() {speed+=10});
 }
 function draw() {
   //handle mouse
-  if (mouseIsPressed) {
+  /*if (mouseIsPressed) {
     speed = (pmouseX-mouseX)/10;
-  }
+  }*/
+
 
 
   background(255);
   translate(width/2, height/2);
-  //rotate((millis()*speed) % 360);
   rotate(rotation);
   image(spinner, - spinner.width/4,- spinner.height/4 , spinner.width/2, spinner.height/2);
   //draw ellipse at mouse position
